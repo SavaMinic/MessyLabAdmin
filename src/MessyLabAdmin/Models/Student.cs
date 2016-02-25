448ba4f65f9d332d234642e2285fb55864008064
savam <savam@nordeus.eu>
2016-02-25 20:34:56 +0100

    Added student model and default controller for it, added initial migration

---
+++ src/MessyLabAdmin/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessyLabAdmin.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int EntryYear { get; set; }
+        public int EntryNumber { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
