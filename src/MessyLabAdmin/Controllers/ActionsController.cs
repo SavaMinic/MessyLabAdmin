323b0938f7f73131a362e00ce2d7d8ec3fb5ced9
savam <savam@nordeus.eu>
2016-04-20 01:37:37 +0200

    Added search input for From/To date and time, included jquery ui

--- src/MessyLabAdmin/Controllers/ActionsController.cs
+++ src/MessyLabAdmin/Controllers/ActionsController.cs
@@ -1,10 +1,13 @@
 using System.Linq;
+using System;
 using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Mvc.Rendering;
 using Microsoft.Data.Entity;
 using MessyLabAdmin.Models;
 using MessyLabAdmin.Util;
 using System.Collections.Generic;
+using System.Globalization;
+using Action = MessyLabAdmin.Models.Action;
 
 namespace MessyLabAdmin.Controllers
 {
@@ -18,9 +21,11 @@ namespace MessyLabAdmin.Controllers
         }
 
         // GET: Actions
-        public IActionResult Index(int? page, int? studentId, int? actionType)
+        public IActionResult Index(int? page, int? studentId, int? actionType, string createdFrom, string createdUntil)
         {
             IQueryable<Action> actions = _context.Actions.Include(a => a.Student);
+            CultureInfo enUS = new CultureInfo("en-US");
+            const string format = "dd.MM.yyyy HH:mm:ss";
 
             if (studentId != null)
             {
@@ -31,6 +36,18 @@ namespace MessyLabAdmin.Controllers
             {
                 actions = actions.Where(a => (int)a.Type == actionType);
                 ViewBag.filteredAction = actionType;
+            }
+            DateTime from;
+            if (createdFrom != null && createdFrom != "" && DateTime.TryParseExact(createdFrom, format, enUS, DateTimeStyles.None, out from))
+            {
+                actions = actions.Where(a => a.CreatedTime >= from);
+                ViewBag.createdFrom = createdFrom;
+            }
+            DateTime until;
+            if (createdUntil != null && createdUntil != "" && DateTime.TryParseExact(createdUntil, format, enUS, DateTimeStyles.None, out until))
+            {
+                actions = actions.Where(a => a.CreatedTime <= until);
+                ViewBag.createdUntil = createdUntil;
             }
             
             ViewBag.currentPage = page ?? 1;
